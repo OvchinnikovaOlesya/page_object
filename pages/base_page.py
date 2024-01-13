58cd1b443713afcd227f9b398c7fad2173784a06
Ovchinnikova Olesya <olesyaryabova815@mail.ru>
2024-01-13 15:26:30 +0300

    adding_exceptions

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,20 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
 class BasePage:
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(10)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how: object, what: object) -> object:
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+
+
