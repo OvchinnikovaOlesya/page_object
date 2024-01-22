2b5f7261fdddf6dbe14f38ce6e9f997a1dea7504
Ovchinnikova Olesya <olesyaryabova815@mail.ru>
2024-01-22 11:42:06 +0300

    adding a registration page

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+
+class LoginPageLocators():
+    login_form = (By.ID, 'login_form')
+    registration_form = (By.ID, 'register_form')
