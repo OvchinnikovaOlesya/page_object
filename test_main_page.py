007ca9b3fdd232303d60d2a55cc13315c41bc73d
Ovchinnikova Olesya <olesyaryabova815@mail.ru>
2024-01-25 20:02:42 +0300

    grouping tests by similar functionality

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,23 @@
+import pytest
 from basket_page import BasketPage
 from .pages.product_page import ProductPage
 from .pages.main_page import MainPage
 
 
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        browser.get(link)
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_login_and_registration_form(browser):
